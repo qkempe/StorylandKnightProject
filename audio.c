c4e9de0ffaf4700c5160f931641dc97d42eb51c1
Quennel K <qkem08@yahoo.com>
2026-05-18 20:39:37 -0700

    Change sleep duration from 6 to 3 seconds
    Reduced sleep duration from 6 seconds to 3 seconds to allow quicker audio sequence restarts.

--- audio.c
+++ audio.c
@@ -96,7 +96,7 @@ void pir_audio_update(PirAudioModule *module) {
 
         // A 6-second cooldown so it doesn't restart the sequence 
         // while the audio is still playing.
-        sleep(6); 
+        sleep(3); 
     }
     module->previous_value = current_value;
 }
