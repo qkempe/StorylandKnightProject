32dd0df344e0a4df06a146b5b9b4c14b9c58c81a
qkempe <qkemp267@gmail.com>
2026-08-16 00:38:13 -0700

    Added some more voice lines for the knight

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #include "pir.h"
 #include "audio.h"
 #include "led.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define COOLDOWN_MS 5000 // 5 second reset window between visitors
 
 int main() {
     PirSensor pir;
+    srand(time(NULL)); //start random seed
 
     if (pir_init(&pir, 17) != 0) {
         fprintf(stderr, "Failed to initialize PIR sensor.\n");
@@ -38,9 +42,33 @@ int main() {
             
             led_on(); // set eyes to red
             
-            // Play audio
-            play_audio_async("/home/diego/StorylandKnightProject/freesound_community-knight-spawn-97118.mp3");
-            play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+            // Play one of the eight audios
+	    int num = rand() % 8;
+	    if (num == 0) {
+            	play_audio_async("/home/diego/StorylandKnightProject/freesound_community-knight-spawn-97118.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 1) {
+		play_audio_async("/home/diego/StorylandKnightProject/phatphrogstudio-rpg-m-wizard-voice-greeting-player-no-ai-479595.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 2) {
+	  	play_audio_async("/home/diego/StorylandKnightProject/phatphrogstudio-rpg-m-knight-voice-stand-ready-477946.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 3) {
+	  	play_audio_async("/home/diego/StorylandKnightProject/phatphrogstudio-rpg-m-knight-voice-attack-grunt-490291.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 4) {
+	  	play_audio_async("/home/diego/StorylandKnightProject/originalvo-medieval-gamer-voice-wisdom-will-come-my-friend-226577.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 5) {
+	  	play_audio_async("/home/diego/StorylandKnightProject/originalvo-medieval-gamer-voice-to-battle-226575.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else if (num == 6) {
+	  	play_audio_async("/home/diego/StorylandKnightProject/originalvo-medieval-gamer-voice-the-day-is-ours-226579.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    } else { // == 7
+	  	play_audio_async("/home/diego/StorylandKnightProject/originalvo-medieval-gamer-voice-darkness-hunts-us-what-youx27ve-learned-stay-226596.mp3");
+            	play_audio_async("/home/diego/StorylandKnightProject/wings_of_freedom-draw-sword-490796.mp3");
+	    }
 
             // Perform movement
             set_servos_open();
